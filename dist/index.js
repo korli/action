b2a99a6121565ff10f8d1c2ed117e5abee93492a
Jérôme Duval <jerome.duval@gmail.com>
2024-03-15 15:05:02 +0100

    Haiku: bump to v0.0.6

--- dist/index.js
+++ dist/index.js
@@ -2490,7 +2490,7 @@ const version = {
         freebsd: 'v0.4.0',
         netbsd: 'v0.2.0',
         openbsd: 'v0.6.0',
-        haiku: 'v0.0.5'
+        haiku: 'v0.0.6'
     },
     resources: 'v0.7.0'
 };
